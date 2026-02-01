85a76a5bc17a7cb36b6fd3703c667f09d1c13be8
Vicky Sharma <vkeysbox@gmail.com>
2026-02-01 23:12:59 +0100

    add hash routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,25 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(
       routes,
-      withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' })
+      withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }),
     ),
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy, // <-- hash routing
+    },
     provideClientHydration(withEventReplay()),
   ],
 };

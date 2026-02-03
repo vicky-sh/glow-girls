7199808978327829edaf722f979513e8566e0c03
Vicky Sharma <vkeysbox@gmail.com>
2026-02-03 20:38:07 +0100

    fix overlay note hide when nav open

--- src/app/home/body/body.ts
+++ src/app/home/body/body.ts
@@ -1,6 +1,8 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, PLATFORM_ID, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AnalyticsService } from '../../analytics-service';
+import { LayoutStateService } from '../../layout-state-service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-body',
@@ -10,8 +12,16 @@ import { AnalyticsService } from '../../analytics-service';
 })
 export class Body implements OnInit {
   public trackingService = inject(AnalyticsService);
+  public layout = inject(LayoutStateService);
+  public menuOpen = this.layout.menuOpen;
+  public isLargeScreen = signal<boolean>(false);
+  private platformId = inject(PLATFORM_ID);
+  private isBrowser = isPlatformBrowser(this.platformId);
 
   ngOnInit(): void {
     this.trackingService.homeViewed();
+    if (this.isBrowser) {
+      this.isLargeScreen.set(window.innerWidth >= 1024);
+    }
   }
 }
